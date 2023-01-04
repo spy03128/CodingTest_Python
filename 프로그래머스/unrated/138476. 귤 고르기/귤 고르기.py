f001f583b13471ae2b531f3ff1407856cd27c3b6
지현 <57143818+spy03128@users.noreply.github.com>
2023-01-04 13:22:05 +0900

    [unrated] Title: 귤 고르기, Time: 28.06 ms, Memory: 14.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,22 @@
+def solution(k, tangerine):
+    answer = 0
+    
+    tangerine.sort()
+    x = tangerine[0]
+    list = []
+    num = 0
+    for i in tangerine:
+        if x == i:
+            num+=1
+        else:
+            list.append(num)
+            x = i
+            num = 1
+    list.append(num)
+    list.sort(reverse=True)
+    if(list[0]>=k): return 1
+    sum,result = 0,0
+    for i in list:
+        sum+=i
+        result+=1
+        if sum >= k: return result
