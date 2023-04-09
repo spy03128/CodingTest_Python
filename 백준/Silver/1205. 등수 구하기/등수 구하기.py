83e12de105937ba57d11f941d9f9e8409c90cc77
지현 <57143818+spy03128@users.noreply.github.com>
2023-04-09 21:49:37 +0900

    [Silver IV] Title: 등수 구하기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -0,0 +1,15 @@
+n, E, p = map(int, input().split())
+
+if n == 0:
+    print(1)
+else:
+    ranking = list(map(int, input().split()))
+    if n == p and ranking[-1] >= E:
+        print(-1)
+    else:
+        rank = n + 1
+        for i in range(n):
+            if ranking[i] <= E:
+                rank = i + 1
+                break
+        print(rank)
