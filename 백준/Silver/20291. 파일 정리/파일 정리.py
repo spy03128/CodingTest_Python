b2da9b84b74fff656f1133519c6e5f88dea0c70f
지현 <57143818+spy03128@users.noreply.github.com>
2023-04-10 21:15:11 +0900

    [Silver III] Title: 파일 정리, Time: 236 ms, Memory: 44316 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -6,11 +6,10 @@ input = sys.stdin.readline
 n=int(input())
 arr = []
 for _ in range(n):
-    s = input().rstrip()
-    for i in range(len(s)):
-        if s[i]=='.':
-            arr.append(s[i+1:])
+    s = input().rstrip().split(".")
+    arr.append(s[1])
 res = Counter(arr)
 res = sorted(res.items())
+
 for x in res:
     print(x[0],x[1])
