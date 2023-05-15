c614daf3fd1ccc18d8c191e39853fbd7f49d606a
지현 <57143818+spy03128@users.noreply.github.com>
2023-05-15 13:50:40 +0900

    [Silver I] Title: 후보 추천하기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+V = int(input())
+students = list(map(int, input().split()))
+picture = []
+score = []
+
+for i in range(V):
+    if students[i] in picture:
+        for j in range(len(picture)):
+            if students[i] == picture[j]:
+                score[j] += 1
+    else:
+        if len(picture) >= N:
+            for j in range(N):
+                if score[j] == min(score):
+                    del picture[j]
+                    del score[j]
+                    break
+        picture.append(students[i])
+        score.append(1)
+
+picture.sort()
+print(' '.join(map(str, picture)))
