efac5b49f4f16195d5a2e5d032ea639fe3ff655f
지현 <57143818+spy03128@users.noreply.github.com>
2023-05-31 10:13:37 +0900

    [Silver IV] Title: 차집합, Time: 828 ms, Memory: 149984 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+na, nb = map(int, input().split())
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+print(len(A - B))
+print(*sorted(list(A - B)))
