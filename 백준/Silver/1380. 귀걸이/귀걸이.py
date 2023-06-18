b2d9e61508483004758aef6557dcc8a01529100b
지 현 (JI HYEON) <57143818+spy03128@users.noreply.github.com>
2023-06-18 19:24:54 +0900

    [Silver V] Title: 귀걸이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1380. 귀걸이/귀걸이.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = 1
+result = []
+while n:
+    n = int(input())
+    names = [input().rstrip() for i in range(n)]
+    values = []
+
+    for i in range(2 * n - 1):
+        a = int(input().split()[0])
+        values.append(a)
+    values.sort()
+    for i in range(0, len(values), 2):
+        if i == (len(values) - 1) or values[i] != values[i + 1]:
+            result.append(names[values[i] - 1])
+            break
+for i in range(len(result)):
+    print(i + 1, result[i])
