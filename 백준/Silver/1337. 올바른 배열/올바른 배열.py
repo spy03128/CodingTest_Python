441ef6bef9aa18982b1118309ae6dfdd5221b6ce
지 현 (JI HYEON) <57143818+spy03128@users.noreply.github.com>
2023-06-25 12:23:38 +0900

    [Silver IV] Title: 올바른 배열, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+nums=[]
+answer=5
+for i in range(n):
+    nums.append(int(input()))
+for i in range(n):
+    cnt1=4
+    cnt2=4
+    for j in range(n):
+        if nums[i]+5>nums[j] and nums[i]<nums[j]:
+            cnt1-=1
+        elif nums[i]-5<nums[j] and nums[i]>nums[j]:
+            cnt2-=1
+    answer=min(answer, cnt1, cnt2)
+print(answer)
